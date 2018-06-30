462e8d6cba346937e6d8b22aaebfa5d9480bdafe
Pedro Pontes <20629214+pedrohpontes@users.noreply.github.com>
2018-06-29 22:22:43 -0400

    Separate into sum of multiples of

--- multiples-of-3-and-5/multiples.go
+++ multiples-of-3-and-5/multiples.go
@@ -1,16 +1,26 @@
 package multiples
 
+func sumUpTo(upperBound int) int {
+	total := 0
+
+	for i := 0; i < upperBound; i++ {
+		total += i
+	}
+
+	return total
+}
+
+func sumOfMultiplesOf(divisor, upperBound int) int {
+	return divisor * sumUpTo((upperBound-1)/divisor+1)
+}
+
 // SumOfMultiplesOf3and5 calculates the sum of all integers between 1 and
 // upperBound, not including upperBound, that are multiples of either 3
 // or 5.
 func SumOfMultiplesOf3and5(upperBound int) int {
-	total := 0
+	sumOfMultiplesOf3 := sumOfMultiplesOf(3, upperBound)
+	sumOfMultiplesOf5 := sumOfMultiplesOf(5, upperBound)
+	sumOfMultiplesOf15 := sumOfMultiplesOf(15, upperBound)
 
-	for i := 1; i < upperBound; i++ {
-		if i%3 == 0 || i%5 == 0 {
-			total += i
-		}
-	}
-
-	return total
+	return sumOfMultiplesOf3 + sumOfMultiplesOf5 - sumOfMultiplesOf15
 }
--- multiples-of-3-and-5/multiples_test.go
+++ multiples-of-3-and-5/multiples_test.go
@@ -6,6 +6,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSumUpTo(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected int
+	}{
+		{0, 0},
+		{1, 0},
+		{2, 1},
+		{3, 3},
+		{4, 6},
+		{10, 45},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expected, sumUpTo(test.input))
+	}
+}
+
+func TestSumOfMultiples(t *testing.T) {
+	testCases := []struct {
+		divisor  int
+		input    int
+		expected int
+	}{
+		{3, 0, 0},
+		{3, 1, 0},
+		{3, 2, 0},
+		{3, 3, 0},
+		{3, 4, 3},
+		{3, 10, 18},
+		{5, 5, 0},
+		{5, 6, 5},
+		{5, 11, 15},
+		{5, 21, 50},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expected, sumOfMultiplesOf(test.divisor, test.input))
+	}
+}
+
 func TestMultiplesOf3and5(t *testing.T) {
 	testCases := []struct {
 		input    int
@@ -19,6 +60,7 @@ func TestMultiplesOf3and5(t *testing.T) {
 		{5, 3},
 		{6, 8},
 		{10, 23},
+		{1000, 233168},
 	}
 
 	for _, test := range testCases {
