2ed840c3b3eb1bd6a389a480c9cfc6c3525d0298
Pedro H. Pontes <20629214+pedrohpontes@users.noreply.github.com>
2018-06-29 22:30:13 -0400

    Add solution to Multiples of 3 and 5 (#1)
    * Basic setup, setting up tests
    
    * Naive solution
    
    * Naive solution
    
    * Separate into sum of multiples of
    
    * Change to sum formula and add test
    
    * Remove unnecessary if clause

---
+++ multiples-of-3-and-5/multiples.go
@@ -0,0 +1,20 @@
+package multiples
+
+func sumUpTo(upperBound int) int {
+	return (upperBound * (upperBound - 1)) / 2
+}
+
+func sumOfMultiplesOf(divisor, upperBound int) int {
+	return divisor * sumUpTo((upperBound-1)/divisor+1)
+}
+
+// SumOfMultiplesOf3and5 calculates the sum of all integers between 1 and
+// upperBound, not including upperBound, that are multiples of either 3
+// or 5.
+func SumOfMultiplesOf3and5(upperBound int) int {
+	sumOfMultiplesOf3 := sumOfMultiplesOf(3, upperBound)
+	sumOfMultiplesOf5 := sumOfMultiplesOf(5, upperBound)
+	sumOfMultiplesOf15 := sumOfMultiplesOf(15, upperBound)
+
+	return sumOfMultiplesOf3 + sumOfMultiplesOf5 - sumOfMultiplesOf15
+}
---
+++ multiples-of-3-and-5/multiples_test.go
@@ -0,0 +1,70 @@
+package multiples
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSumUpTo(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected int
+	}{
+		{0, 0},
+		{1, 0},
+		{2, 1},
+		{3, 3},
+		{4, 6},
+		{10, 45},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expected, sumUpTo(test.input))
+	}
+}
+
+func TestSumOfMultiples(t *testing.T) {
+	testCases := []struct {
+		divisor  int
+		input    int
+		expected int
+	}{
+		{3, 0, 0},
+		{3, 1, 0},
+		{3, 2, 0},
+		{3, 3, 0},
+		{3, 4, 3},
+		{3, 10, 18},
+		{5, 5, 0},
+		{5, 6, 5},
+		{5, 11, 15},
+		{5, 21, 50},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expected, sumOfMultiplesOf(test.divisor, test.input))
+	}
+}
+
+func TestMultiplesOf3and5(t *testing.T) {
+	testCases := []struct {
+		input    int
+		expected int
+	}{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 3},
+		{5, 3},
+		{6, 8},
+		{10, 23},
+		{1000, 233168},
+		{10000, 23331668},
+	}
+
+	for _, test := range testCases {
+		assert.Equal(t, test.expected, SumOfMultiplesOf3and5(test.input))
+	}
+}
