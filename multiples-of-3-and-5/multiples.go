ee322ca904772111072106757954fbfed60ded0e
Pedro Pontes <20629214+pedrohpontes@users.noreply.github.com>
2018-06-29 22:04:28 -0400

    Naive solution

--- multiples-of-3-and-5/multiples.go
+++ multiples-of-3-and-5/multiples.go
@@ -1,5 +1,8 @@
 package multiples
 
+// SumOfMultiplesOf3and5 calculates the sum of all integers between 1 and
+// upperBound, not including upperBound, that are multiples of either 3
+// or 5.
 func SumOfMultiplesOf3and5(upperBound int) int {
 	total := 0
 
